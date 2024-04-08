016ad31a7b6d30eba713266bec026c2ec978edec
bardoor <dmitrybartenev58@gmail.com>
2024-04-08 14:09:33 +0300

    10: changed d calc in findRoutes

--- app/Main.hs
+++ app/Main.hs
@@ -30,7 +30,7 @@ fastFactorial n = helper 1 n
 findRoutes :: Floating a => a -> a -> a -> (a, a)
 findRoutes a b c = (x1, x2)
     where 
-        d = sqrt $ b ** 2 - 4 * a * c
+        d = sqrt $ b ** 3 - 4 * a * c
         doubleA = 2 * a 
         x1 = (-b + d) / doubleA
         x2 = (-b - d) / doubleA
