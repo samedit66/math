b524f3ff56b7b130375a9c760e40c7536daf25e3
bardoor <dmitrybartenev58@gmail.com>
2024-04-08 13:55:39 +0300

    init

---
+++ app/Main.hs
@@ -0,0 +1,47 @@
+module Main where
+
+main :: IO ()
+main = putStrLn "Hello, Haskell!"
+
+fastFibonacci :: Integer -> Integer
+fastFibonacci n = helper (0, 1) n 
+    where helper (r2, r1) n | n == 0 = r2
+                            | n == 1 = r1
+                            | n > 1  = helper (r1, r2 + r1) (n - 1) 
+                            | otherwise  = helper (r1 - r2, r2) (n + 1) 
+
+fibonacci :: Integer -> Integer
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci n = fibonacci (n - 1) + fibonacci (n - 2)
+
+factorial :: Integer -> Integer
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+prodFactorial :: Integer -> Integer
+prodFactorial n = product [1..n]
+
+fastFactorial :: Integer -> Integer
+fastFactorial n = helper 1 n
+    where helper acc n | n == 0 = acc
+                       | otherwise = helper (acc * n) (n - 1)
+
+findRoutes :: Floating a => a -> a -> a -> (a, a)
+findRoutes a b c = (x1, x2)
+    where 
+        d = sqrt $ b ** 2 - 4 * a * c
+        doubleA = 2 * a 
+        x1 = (-b + d) / doubleA
+        x2 = (-b - d) / doubleA
+
+-- Метод трапеций с разбиением на 100000 сегментов
+integration :: (Double -> Double) -> Double -> Double -> Double
+integration f a b | a /= b = step * ((a_val + b_val) / 2 + func_sum)
+                  | otherwise = 0
+    where
+        a_val = f a
+        b_val = f b
+        func_sum = sum $ map f [a, a + step .. b - step]
+        step = (b - a) / 100000
+        
